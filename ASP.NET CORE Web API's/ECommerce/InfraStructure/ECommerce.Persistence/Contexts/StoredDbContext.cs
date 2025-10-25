15314afda159654170a3ed085793750aabe30aea
Alyaa <alyaatamer88@gmail.com>
2025-10-25 12:29:45 +0300

    ......

---
+++ ASP.NET CORE Web API's/ECommerce/InfraStructure/ECommerce.Persistence/Contexts/StoredDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Domain.Models.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Contexts
+{
+    public class StoredDbContext : DbContext
+    {
+
+        public StoredDbContext(DbContextOptions<StoredDbContext> options): base(options) 
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoredDbContext).Assembly);
+        }
+
+        public DbSet<Product> Products { get; set; } 
+        public DbSet<ProductBrand> ProductBrands { get; set; } 
+        public DbSet<ProductType> ProductTypes { get; set; } 
+    }
+}
---
+++ ASP.NET CORE Web API's/ECommerce/InfraStructure/ECommerce.Persistence/Seed/DataSeeding.cs
@@ -0,0 +1,66 @@
+﻿using ECommerce.Domain.Contracts.Seed;
+using ECommerce.Domain.Models.Products;
+using ECommerce.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Seed
+{
+    public class DataSeeding : IDataSeeding
+    {
+        private readonly StoredDbContext context;
+
+        public DataSeeding(StoredDbContext context)
+        {
+            this.context = context;
+        }
+        public void DataSeed()
+        {
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if(!context.ProductBrands.Any())
+            {
+                var BrandData = File.ReadAllText(@"..\InfraStructure\ECommerce.Persistence\Data\brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(BrandData);
+
+                if(brands != null && brands.Any())
+                {
+                    context.ProductBrands.AddRange(brands);
+                }
+            }
+
+            if (!context.ProductTypes.Any())
+            {
+                var TypeData = File.ReadAllText(@"..\InfraStructure\ECommerce.Persistence\Data\types.json");
+                var Types = JsonSerializer.Deserialize<List<ProductType>>(TypeData);
+
+                if (Types != null && Types.Any())
+                {
+                    context.ProductTypes.AddRange(Types);
+                }
+            }
+
+            if (!context.Products.Any())
+            {
+                var ProductsData = File.ReadAllText(@"..\InfraStructure\ECommerce.Persistence\Data\products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(ProductsData);
+
+                if (products != null && products.Any())
+                {
+                    context.Products.AddRange(products);
+                }
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
